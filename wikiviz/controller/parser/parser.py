fb7fb2e34b65678a06225cd528eebfd699d544df
skmullins <skmllns@gmail.com>
2014-03-01 10:56:04 -0500

    Additional filtered keywords, search term extraction
    Extract related search term from hyperlinks

--- wikiviz/controller/parser/parser.py
+++ wikiviz/controller/parser/parser.py
@@ -19,70 +19,117 @@ class Parser():
         print "BeautifulSoup imported"
         print "Parser created"
         
-       
+    filtered_keywords = ('Help:', 'Category:', 'Talk:', 'Special:', 'Wikipedia:', 'bits.wikimedia.org', 'File:', 'en/thumb/', '.svg.', 'Portal:', 'Template:', '/Main_Page')
+
+    """ 
+    class definitions 
+    
+    """
+    
+    class PageLink:
+        
+        def __init__(self, page_url, page_priority):
+            self.page_url = page_url
+            self.page_priority = page_priority
+            
+    class ImageLink:
+        
+        def __init__(self, image_url, image_priority):
+            self.image_url = image_url
+            self.image_priority = image_priority        
+            
+    
+    """ 
+    function definitions
+    
+    """
+    
     """
     get links
+    page and image links are currently separate
     """
+    
     def get_links(soup):
-    
+        
         link_list = list()
-    
+        
         """ use keywords list to filter out undesirable elements"""
-        filtered_keywords = ('Help:', 'Category:', 'Talk:', 'Special:', 'Wikipedia:', 'File:')
-    
+        
+        
         """ get links from wiki article that have a type and are NOT anchors """
         for anchor in soup.find_all('a'):
-            temp_link = anchor.get('href')
-            if type(temp_link) is not NoneType and temp_link.startswith('/wiki/'):
+            page_url = anchor.get('href')
+            
+            """ initially set priority to 3, just for starters"""
+            temp_link = PageLink(page_url, 3)
+            if type(temp_link.page_url) is not NoneType and temp_link.page_url.startswith('/wiki/'):
                 link_list.append(temp_link)
-    
+        
             for keyword in filtered_keywords:
                 for item in link_list:
-                    if keyword in item:
+                    if keyword in item.page_url:
                         link_list.remove(item)
-       
+        
         """create distinct set of links"""
-        distinct_link_list = list(set(link_list))
+        """ do this after prioritizing! """
+        #distinct_link_list = list(set(link_list))
         
         """ print list """
-        for elem in distinct_link_list:
-            print elem 
-     
+        for elem in link_list:
+            print elem.page_url
+            
+        return link_list
+            
+        
+            
+    """
+    get word alone
+    
+    """
+    def get_link_word(link_list):
+    
+        link_word_list = list()
+        
+        for word in link_list:
+            link_word = PageLink(word.page_url, 3)
+            
+            link_word_list.append(word.page_url[6:])
+            
+            
+        ##hmm but you want the url too, right?
+            
+        for link_word in link_word_list:
+            if "_" in link_word:
+                print link_word
+             
+            
     """
     get images: 
     ignore //bits.wikimedia.org
     only use //upload.wikimedia.org
-
+    
     """
-
+    
     def get_pics(soup):
         pic_list = list()
-    
+        
         """ add only images themselves to list """
         for image in soup.find_all('img'):
-            temp_img = image.get('src')
-            if 'bits.wikimedia.org' in temp_img:
+            image_url = image.get('src')
+            temp_img = ImageLink(image_url, 3)
+            if 'bits.wikimedia.org' in temp_img.image_url:
                 continue
             else:
                 pic_list.append(temp_img)
-         
+            
+            for keyword in filtered_keywords:
+                for item in pic_list:
+                    if keyword in item.image_url:
+                        pic_list.remove(item)
+             
         """ print image links """     
         for item in pic_list:
-            print item
+            print item.image_url
 
-    """
-    end function defs
-    """
-
-
-    """ Create BeautifulSoup object from given HTML page and specify unicode version """
-    
-    """ TODO: try out with many different Wikipedia pages; currently have Knitting and Rockabilly """
-
-    soup = BeautifulSoup(open("rockabilly.html"), from_encoding="UTF-8")
-
-    get_links(soup)
-
-    #get_pics(soup)
-
-
+   
+  
