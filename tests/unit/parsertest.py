922fb9cee947ab7f5516cffb38342d5edc275e91
ashleyrevlett <ashleyrevlett@gmail.com>
2014-03-04 12:19:12 -0500

    fixed parser tests

--- tests/unit/parsertest.py
+++ tests/unit/parsertest.py
@@ -1,12 +1,26 @@
-
+import unittest
 from bs4 import BeautifulSoup
-from parser import parser
+import wikiviz.controller.parser.parser as parser
+import os
 
-soup = BeautifulSoup(open("knitting.html"), from_encoding="UTF-8")
- 
-  
-"""print out page and image links """
-print "PAGE LINKS"
-print "**********"
+class ParserTests(unittest.TestCase):
+	
+	def test_init(self):
+		# you need the full path for the file to open:
+		current_path = os.path.dirname(os.path.realpath(__file__))
+		soup = BeautifulSoup(open(current_path+"/knitting.html"), from_encoding="UTF-8")
+		 
+		  
+		"""print out page and image links """
+		print "PAGE LINKS"
+		print "**********"
 
-parser(soup)
+		# parser's constructor does not currently accept a parameter
+		p = parser.Parser()
+
+		# test that p is not empty
+		self.assertTrue(p)
+
+	
+if __name__ == '__main__':
+	unittest.main()
