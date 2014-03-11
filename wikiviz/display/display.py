b0758c72d27b90396bac4df8b759cf54b9164dd8
enrush01 <enrush01@louisville.edu>
2014-03-10 23:49:04 -0400

    new search functionality. scaling on buttons is better.

--- wikiviz/display/display.py
+++ wikiviz/display/display.py
@@ -3,6 +3,7 @@ kivy.require('1.8.0')
 
 from random import random 
 from kivy.app import App
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -20,44 +21,80 @@ from kivy.graphics.stencil_instructions import StencilPush, StencilPop, StencilU
 
 
 
-
 class UIC(ScatterPlane):
     uis = ObjectProperty(None)
+    uibc = ObjectProperty(None)
 
-    abs_sx = NumericProperty(0)
-    abs_sy = NumericProperty(0)
+    Nodes = ListProperty(None)
+
+
+    abs_l = NumericProperty(0)
+    abs_h = NumericProperty(0)
+
+
 
     def __init__(self, **kwargs):
         super(ScatterPlane, self).__init__(**kwargs)
         
+
+
+        
+        self.uibc.disabled = True
+        self.uibc.opacity = 0
+  
+  
+
         self.do_rotation = False
         self.do_scale = False
         self.do_translation= False
+        
    
      
     def display(self):   
-        self.remove_widget(self.uis)
+        print " KEYWORD=", self.uis.search_bar.text 
+        self.uis.opacity = 0
+        self.uis.disabled= True
+        
+        self.uibc.opacity = 100
+        self.uibc.disabled= False
+
         self.do_scale = True
         self.do_translation= True
 
-        node = NodeWithBkgrd(size=(100,100), pos=(self.center_x-50,self.center_y-50))
+        node = NodeWithBkgrd(size=(100,100), pos=(0,0))
         self.add_widget(node)
         node1 = NodeWithBkgrd(size=(100,100), pos=(800,800))
         self.add_widget(node1)
         node.display("http://i1164.photobucket.com/albums/q572/marshill2/sun_zps0fa10dc5.jpg")
         node1.display("http://i1164.photobucket.com/albums/q572/marshill2/sun_zps0fa10dc5.jpg")
+        self.connect_with_line((0,0), (800,800))
 
-        self.connect_with_line(node, node1)
-
-    def connect_with_line(self,node1, node2):
+    def add_node(self, node):
+        self.add_widget(node)
+    def connect_with_line(self,coord1, coord2):
         with self.canvas:
             Color = (random(),random(),random())
-            Line(points = [node1.center_x,node1.center_y,node2.center_x,node2.center_y], width=2)
-        self.remove_widget(node1)
-        self.remove_widget(node2)
-        self.add_widget(node1)
-        self.add_widget(node2)
+            Line(points = [coord1[0], coord1[1], coord2[0], coord2[1]], width=2)
 
+    def new_search(self):
+        self.scale = 1.0
+       
+  
+        self.remove_widget(self.uibc)
+        self.remove_widget(self.uis)
+        self.canvas.clear()
+        self.pos = (0,0)
+
+ 
+        self.add_widget(self.uibc)
+        self.add_widget(self.uis)
+       
+        self.uis.opacity = 100
+        self.uis.disabled = False
+        self.uibc.opacity = 0
+        self.uibc.disabled= True
+        self.do_scale = False
+        self.do_translation= False
 
     def on_transform_with_touch(self, touch):
         return
@@ -87,7 +124,7 @@ class NodeWithBkgrd(Widget):
         return
     def on_touch_down(self, touch):
         print self.pos[0], self.pos[1]
-        if (abs(touch.x - self.center_x) <=(self.width) and abs(touch.y - self.center_y) <= (self.height)):
+        if (abs(touch.x - self.pos[0]) <=2*(self.width) and abs(touch.y - self.pos[1]) <= 2*(self.height)):
             print "here"
         return
 
@@ -116,7 +153,7 @@ class UITextInput(TextInput):
         return
 
 
-class UISearchButton(Button):
+class UISearchButton(Image, ButtonBehavior):
 
     def on_touch_down(self,touch):
         if (abs(touch.x - self.center_x) <=self.width/2 and abs(touch.y - self.center_y) <= self.width/2):
@@ -125,20 +162,53 @@ class UISearchButton(Button):
             uic.display()
         return
 
+class UINSearchButton(Image, ButtonBehavior):
+    def on_touch_down(self,touch):
+        if (abs(touch.x - self.center_x) <=self.width/2 and abs(touch.y - self.center_y) <= self.width/2):
+            uis = self.parent
+            uic = uis.parent
+            uic.new_search()
+            
+        return
+
+class UIShare(Image, ButtonBehavior):
+    def on_touch_down(self,touch):
+        if (abs(touch.x - self.center_x) <=self.width/2 and abs(touch.y - self.center_y) <= self.width/2):
+            uis = self.parent
+            uic = uis.parent  
+            
+        return
+
+class UIBContainer(BoxLayout):
+    new_search = ObjectProperty(None)
+    share = ObjectProperty(None)
+
+    window_c_x = NumericProperty(0)
+    window_c_y = NumericProperty(0)
 
 
-class WikiVizApp(App):
-    
 
+    def upper_left(self, w , h, s):
+
+        parent = self.parent
+
+        w_pos = self.to_window(self.x, self.y)
+
+        self.pos = self.x + (self.window_c_x - w_pos[0])/s, self.y+(self.window_c_y - w_pos[1])/s
+        parent.remove_widget(self)
+        parent.add_widget(self)
+
+
+class DisplayApp(App):
 
 
     def build(self):
-        
-        bkgrd = UIC()       
+        bkgrd = UIC()
+ 
        
         return bkgrd
 
 
 if __name__=='__main__':
-    WikiVizApp().run()
+    DisplayApp().run()
         
