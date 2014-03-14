23d2a5cff9e99bd8200daf97738f6f13c10ff226
enrush01 <enrush01@louisville.edu>
2014-03-14 19:39:54 -0400

    easier to follow code

--- wikiviz/display/display.py
+++ wikiviz/display/display.py
@@ -8,6 +8,7 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
 from kivy.uix.scatter import ScatterPlane
 from kivy.uix.image import Image
@@ -25,181 +26,149 @@ class UIC(ScatterPlane):
     uis = ObjectProperty(None)
     uibc = ObjectProperty(None)
 
-    Nodes = ListProperty(None)
-
-
-    abs_l = NumericProperty(0)
-    abs_h = NumericProperty(0)
+    uipup = ObjectProperty(None)
 
 
 
     def __init__(self, **kwargs):
+        self.register_event_type('on_add_node')
         super(ScatterPlane, self).__init__(**kwargs)
         
-
-
         
         self.uibc.disabled = True
-        self.uibc.opacity = 0
-  
-  
+
 
         self.do_rotation = False
         self.do_scale = False
         self.do_translation= False
+
+
+        self.remove_widget(self.uibc)
+        self.remove_widget(self.uipup)
+
         
+
+        return
+
    
      
     def display(self):   
         print " KEYWORD=", self.uis.search_bar.text 
-        self.uis.opacity = 0
-        self.uis.disabled= True
-        
-        self.uibc.opacity = 100
+        self.remove_widget(self.uis)
+ 
         self.uibc.disabled= False
 
         self.do_scale = True
         self.do_translation= True
+        self.uis.disabled= True
 
-        node = NodeWithBkgrd(size=(100,100), pos=(0,0))
+
+        self.add_widget(self.uibc)
+        
+
+
+        '''
+                    This code will be deleted when model is hoooked up
+        '''
+        node = Node(size=(100,100), pos=(0,0))
         self.add_widget(node)
-        node1 = NodeWithBkgrd(size=(100,100), pos=(800,800))
+        node1 = Node(size=(100,100), pos=(800,800))
         self.add_widget(node1)
         node.display("http://i1164.photobucket.com/albums/q572/marshill2/sun_zps0fa10dc5.jpg")
         node1.display("http://i1164.photobucket.com/albums/q572/marshill2/sun_zps0fa10dc5.jpg")
         self.connect_with_line((0,0), (800,800))
 
-    def add_node(self, node):
+        '''
+                            END DELETEION
+        '''
+
+
+    def on_add_node(self, model_node):
+        to_be_added = NodeWithBkgrd(model_node.get_pos())
+        to_be_added.display(model_node.get_source())
         self.add_widget(node)
+
     def connect_with_line(self,coord1, coord2):
         with self.canvas:
             Color = (random(),random(),random())
             Line(points = [coord1[0], coord1[1], coord2[0], coord2[1]], width=2)
 
+    def confirm_new_search(self):
+        self.add_widget(self.uipup)
+
     def new_search(self):
+
         self.scale = 1.0
        
-  
         self.remove_widget(self.uibc)
         self.remove_widget(self.uis)
+        self.remove_widget(self.uipup)
         self.canvas.clear()
         self.pos = (0,0)
-
- 
-        self.add_widget(self.uibc)
         self.add_widget(self.uis)
-       
-        self.uis.opacity = 100
+
         self.uis.disabled = False
-        self.uibc.opacity = 0
         self.uibc.disabled= True
+
+        self.uis.search_bar.font_size = self.uis.height - 20
+        self.uis.search_bar.font_name = 'DroidSans'
+
         self.do_scale = False
         self.do_translation= False
 
-    def on_transform_with_touch(self, touch):
-        return
+        self.uis.search_bar.text  = 'Search a Word'
 
-    def on_touch_down(self, touch):
-        print "scale =" , self.scale
-        print " touch down"
-        super(ScatterPlane, self).on_touch_down(touch)
-        return
-    def on_touch_move(self, touch):
-        print "scale =" , self.scale
-        print "on touch move"
-        print touch
-        super(ScatterPlane, self).on_touch_move(touch)
-        return
-    def on_touch_up(self, touch):
-        print " touch up"
-        super(ScatterPlane, self).on_touch_up(touch)
         return
 
 
-class NodeWithBkgrd(Widget):
-    node = ObjectProperty(None)
+
+ 
+
+
+'''
+                                NODE CLASSES
+'''
+
+class Node(Widget):
+    asynci = ObjectProperty(None)
     def display(self, link):
-        self.node.display(link)
-        self.node.pos = self.pos
+        self.source = link
         return
+
     def on_touch_down(self, touch):
         print self.pos[0], self.pos[1]
         if (abs(touch.x - self.pos[0]) <=2*(self.width) and abs(touch.y - self.pos[1]) <= 2*(self.height)):
             print "here"
         return
 
+     
+'''
+                            END NODE CLASSES
+'''   
 
-class Node(AsyncImage):
 
-
-    def display(self, link):
-        self.source= link      
-        return
-        
-
-    
-    
-class UISearch(Widget):
-    search_bar = ObjectProperty(None)
-    go_button = ObjectProperty(None)
+'''
+                        UI INITIAL SEARCH CLASSES
+'''
 
 
 class UITextInput(TextInput):
 
-
     def on_touch_up(self, touch):
-        self.text = ''
-        super(TextInput, self).on_touch_up(touch)
+        if (abs(touch.x - self.center_x) <=self.width/2 and abs(touch.y - self.center_y) <= self.height/2):
+            self.text = ''
+            super(TextInput, self).on_touch_up(touch)
         return
 
 
-class UISearchButton(Image, ButtonBehavior):
-
-    def on_touch_down(self,touch):
-        if (abs(touch.x - self.center_x) <=self.width/2 and abs(touch.y - self.center_y) <= self.width/2):
-            uis = self.parent
-            uic = uis.parent
-            uic.display()
-        return
-
-class UINSearchButton(Image, ButtonBehavior):
-    def on_touch_down(self,touch):
-        if (abs(touch.x - self.center_x) <=self.width/2 and abs(touch.y - self.center_y) <= self.width/2):
-            uis = self.parent
-            uic = uis.parent
-            uic.new_search()
-            
-        return
-
-class UIShare(Image, ButtonBehavior):
-    def on_touch_down(self,touch):
-        if (abs(touch.x - self.center_x) <=self.width/2 and abs(touch.y - self.center_y) <= self.width/2):
-            uis = self.parent
-            uic = uis.parent  
-            
-        return
-
-class UIBContainer(BoxLayout):
-    new_search = ObjectProperty(None)
-    share = ObjectProperty(None)
-
-    window_c_x = NumericProperty(0)
-    window_c_y = NumericProperty(0)
+'''
+                        END UI INITIAL SEARCH CLASSES
+'''
 
 
 
-    def upper_left(self, w , h, s):
 
-        parent = self.parent
-
-        w_pos = self.to_window(self.x, self.y)
-
-        self.pos = self.x + (self.window_c_x - w_pos[0])/s, self.y+(self.window_c_y - w_pos[1])/s
-        parent.remove_widget(self)
-        parent.add_widget(self)
-
-
-class DisplayApp(App):
+class WikiVizApp(App):
 
 
     def build(self):
@@ -210,5 +179,5 @@ class DisplayApp(App):
 
 
 if __name__=='__main__':
-    DisplayApp().run()
+    WikiVizApp().run()
         
