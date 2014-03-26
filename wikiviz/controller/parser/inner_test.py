735afed698b4bfcb1c4a81a925d83f0e26e1f2e5
skmullins <skmllns@gmail.com>
2014-03-26 17:41:34 -0400

    trying to make parser classes easier to understand

--- wikiviz/controller/parser/inner_test.py
+++ wikiviz/controller/parser/inner_test.py
@@ -1,19 +1,30 @@
-import unittest
-from bs4 import BeautifulSoup
+
 import wikiviz.controller.parser.parser as parser
 import os
-import urllib
+from bs4 import BeautifulSoup
 
-f = open('workfile.txt', 'w')
+page_data = open('page_data.txt', 'w')
 
-
+#remove, once hooked up with network
 current_path = os.path.dirname(os.path.realpath(__file__))
-soup = BeautifulSoup(open(current_path + "/knitting.html"), from_encoding="UTF-8")
+soup = BeautifulSoup(open(current_path + "\knitting.html"), from_encoding="UTF-8")
 
 p = parser.Parser()
 
-link_list, pic_list = p.get_links(soup)
+link_list = p.get_links(soup)
+
+image_list = p.get_images(soup)
+
 p_link_list = p.prioritize_links(link_list, "Knitting")
-distinct_link_set = p.remove_duplicates(p_link_list)
-for item in distinct_link_set:
-    f.write(item.page_url.encode('utf8')+'\n')
+
+
+#distinct_link_set = p.remove_duplicates(p_link_list)
+
+
+#for item in distinct_link_set:
+ #   page_data.write(item.page_url.encode('utf8')+'\n')
+#
+# page_data.write("***************************" + '\n')
+#
+# for pic in pic_list:
+#     page_data.write(pic.page)
