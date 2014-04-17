49edd44ddbd372c79f419a0faa3d83ee31dbf3b5
ashleyrevlett <ashleyrevlett@gmail.com>
2014-04-16 21:34:23 -0400

    cleaned up network code. hooked up network to parser but parser is not returning any data currently. app crashes once null data is returned to the ui node.

--- tests/functional_tests/test_functional.py
+++ tests/functional_tests/test_functional.py
@@ -1,6 +1,7 @@
 """
     integration and end-to-end tests
-    run with 'nosetests test_functional.py'
+    run with 'nosetests -s test_functional.py'
+    the -s flag allows print statements to work
 """
 
 import unittest
@@ -10,13 +11,22 @@ import wikiviz.controller.network.network as network
 class FunctionalTests(unittest.TestCase):
     def setUp(self):
         "set up test fixtures; run automatically"
-        self.wnetwork = network.NetworkRequest(None, None)
+        self.wnetwork = network.NetworkRequest(None, self.testNetworkSuccess)
 
     def testNetwork(self):
+        print "testing network init"
         self.assertTrue(self.wnetwork)
 
     def testNetworkGetPage(self):
+        print "testing network get page"
         self.wnetwork.get_page("Pacific_Southwest_Airlines")
+
+    def testNetworkSuccess(self):
+        print "network response returned"
+        # self.assertTrue(completed_request)
+        # self.assertTrue(model_node)
+
+
 
 if __name__ == '__main__': 
     unittest.main() 
