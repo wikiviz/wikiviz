b705756a17b4d1f7bcaa77589d22b876538b6c39
skmullins <skmllns@gmail.com>
2014-04-22 12:49:07 -0400

    trying to implement picture icon

--- wikiviz/SummaryBox.py
+++ wikiviz/SummaryBox.py
@@ -10,6 +10,8 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.properties import StringProperty
+from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
 from controller.parser import parser
 
 #remove once we can get the summary from the network
@@ -18,13 +20,19 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 url = current_path + "\Knitting.html"
 opened_url = open(url)
 
+#get images
 p = parser.Parser(raw_page_content=opened_url)
-p.get_text_summary()
-summary = p.wrapped_summary
+img_srcs = p.get_images(5)
+main_image = img_srcs[0]
 
-class SummaryBox(Widget):
+#get text summary
+#currently doesn't maintain all the '\n', not sure why atm
+summary = p.get_text_summary()
+
+class SummaryBox(GridLayout):
         summary_box = StringProperty(summary)
-        
+        image = ObjectProperty(main_image)
+
 class SummaryApp(App):
     def build(self):
         return SummaryBox()
